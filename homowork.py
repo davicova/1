46f29137c480bdbbeebf06ea195248478b4c0fc4
davicova <86981409+davicova@users.noreply.github.com>
2021-07-08 14:59:58 -0300

    Add files via upload

---
+++ homowork.py
@@ -0,0 +1,17 @@
+nome="davi"
+print(nome)
+print(nome.upper())
+bixo="NAUE"
+print(bixo)
+print(bixo.lower())
+nome="davi"
+nome2="cova"
+nome3=nome+" "+nome2+"!"
+print(nome3.title())
+print("davi\n\tcova")
+comida="  miojo  "
+print(comida+"!")
+print(comida.strip()+"!")
+print("\\tetris")
+print(654*177)
+print(102/23)
